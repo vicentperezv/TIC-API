2ee64be7799807fa5843f25d298a80bf538bdff9
Vicente Perez <Vicentperezv@gmail.com>
2024-11-08 17:09:22 -0300

    add endpoint

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from fastapi import FastAPI, HTTPException
 from typing import Union
-from database import db
+from database import sensor_collection
 from models.sensor_data import SensorData
+from typing import List
+
 app = FastAPI()
 
 @app.post("/sensor-data")
 async def add_sensor_data(data: SensorData):
     try:
         # Guarda los datos en la colección "sensor_data"
-        result = await db["sensor_data"].insert_one(data.dict())
+        result = await sensor_collection.insert_one(data.dict())
         return {"status": "Data inserted", "id": str(result.inserted_id)}
     except Exception as e:
         raise HTTPException(status_code=500, detail="Error inserting data")
+
+# Endpoint para obtener las últimas 10 inserciones
+@app.get("/sensor-data/recent", response_model=List[SensorData])
+async def get_recent_sensor_data():
+    try:
+        # Obtiene los últimos 10 documentos, ordenados por timestamp en orden descendente
+        recent_data = await sensor_collection.find().sort("timestamp", -1).limit(10).to_list(length=10)
+        return recent_data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error retrieving data")
 
 
 @app.get("/")
