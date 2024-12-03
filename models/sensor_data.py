fb14babb97517138d5de9f451ff4f3a8ee0b9713
Vicente Perez <Vicentperezv@gmail.com>
2024-12-03 16:41:57 -0300

    fix sensor data model

--- models/sensor_data.py
+++ models/sensor_data.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
 class SensorData(BaseModel):
-    temperature: float
-    noise: float
+    temperature: Optional[float] = None
+    noise: Optional[float]
     light: Optional[float] = None
     timestamp: datetime = datetime.now()
